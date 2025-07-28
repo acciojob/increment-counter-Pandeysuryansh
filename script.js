0cead08c40341b763e6dd5e4486bfdfd512d8b1d
Pandeysuryansh <167824825+Pandeysuryansh@users.noreply.github.com>
2025-07-28 22:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const counterpara = document.getElementById('counter');
+		const incrementbtn = document.getElementById('incrementBtn');
+
+		incrementbtn.AddEventListener('click' , function(){
+         const cnt = parseInt(counterpara.textContent);
+
+			alert("Current value: " + cnt);
+
+			counterpara.textContent = cnt + 1;
+		})
